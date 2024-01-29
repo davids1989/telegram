2ef573f1b5184322e70ea413328272dc90a727e0
davids1989 <daviddegusmao@gmail.com>
2024-01-29 11:54:23 -0300

    permissões 02

--- teletriade3.py
+++ teletriade3.py
@@ -5,7 +5,7 @@ import requests
 import logging
 import httpx
 from telegram import ChatPermissions
-from telegram.ext import ChatMemberUpdated
+from telegram.ext import ChatMemberUpdate
 
 # Configure httpcore logging to suppress INFO messages
 httpx_logger = logging.getLogger('httpx')
