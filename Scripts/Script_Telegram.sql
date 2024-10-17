1d055f26becd1c8b560bb143cd212632645567dd
TI Triade <daviddegusmao@gmail.com>
2024-10-17 15:10:27 -0300

    1

--- Scripts/Script_Telegram.sql
+++ Scripts/Script_Telegram.sql
@@ -1,6 +1,8 @@
 SELECT * FROM usuariosgrupos where telegram_id = 1292043080 and grupo = "suporte_group";
 select * from grupos;
 
+SELECT * FROM usuarios;
+
 ALTER TABLE usuarios MODIFY COLUMN id INT;
 
 ALTER TABLE usuarios CHANGE id telegram_id INT;
@@ -12,4 +14,7 @@ ALTER TABLE usuarios DROP PRIMARY KEY;
 ALTER TABLE usuarios ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY;
 
 
-INSERT INTO usuarios (username, grupo, telegram_id) VALUES ('testeuser', 'grupo_teste', '123456')
+INSERT INTO usuarios (username, grupo, telegram_id) VALUES ('testeuser', 'grupo_teste', '123456');
+
+SELECT * FROM leads WHERE ativo = 1;
+SELECT id_mensagens, texto, dias FROM remarketing_mensagens;
