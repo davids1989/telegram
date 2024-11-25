d2097ffd3bab91231f6b3fd2f6447e66f9b0117f
TI Triade <daviddegusmao@gmail.com>
2024-11-25 16:38:33 -0300

    10

--- api/teletriade.js
+++ api/teletriade.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 
 
@@ -16,16 +16,14 @@ const connection = mysql.createPool({
   database: 'telegram',
 });
 
-//connection.connect((err) => {
-//  if (err) throw err;
-//  console.log('conexão com o MySQL estabelecida com sucesso!!');
-//});
-
 // API PARA O TELEGRAM
 
 app.get('/api/usuarios/', (req, res) => {
-  connection.query('SELECT * FROM usuarios', (err, result) => {
-    if (err) throw err;
+  pool.query('SELECT * FROM usuarios', (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Erro ao buscar usuários.');
+    }
     res.send(result);
   });
 });
@@ -34,33 +32,55 @@ app.post('/api/usuarios', (req, res) => {
   const { username, grupo, telegram_id } = req.body;
 
   // Verificar se o telegram_id já existe
-  connection.query('SELECT COUNT(*) as count FROM usuarios WHERE telegram_id = ? AND grupo = ?', [telegram_id, grupo], (selectErr, selectResult) => {
-    if (selectErr) throw selectErr;
+  pool.query(
+    'SELECT COUNT(*) as count FROM usuarios WHERE telegram_id = ? AND grupo = ?', 
+    [telegram_id, grupo], 
+    (selectErr, selectResult) => {
+      if (selectErr) {
+        console.error(selectErr);
+        return res.status(500).send('Erro ao verificar usuário.');
+      }
 
-    const userCount = selectResult[0].count;
+      const userCount = selectResult[0].count;
 
-    if (userCount > 0) {
-      // O usuário já existe no grupo
-      res.status(400).send('Usuário já existe no grupo.');
-    } else {
-      // O usuário não existe, proceder com a inserção
-      connection.query('INSERT INTO usuarios (username, grupo, telegram_id) VALUES (?, ?, ?)', [username, grupo, telegram_id], (insertErr, insertResult) => {
-        if (insertErr) throw insertErr;
-        res.send('Usuário Salvo!');
-      });
+      if (userCount > 0) {
+        // O usuário já existe no grupo
+        return res.status(400).send('Usuário já existe no grupo.');
+      } else {
+        // O usuário não existe, proceder com a inserção
+        pool.query(
+          'INSERT INTO usuarios (username, grupo, telegram_id) VALUES (?, ?, ?)', 
+          [username, grupo, telegram_id], 
+          (insertErr, insertResult) => {
+            if (insertErr) {
+              console.error(insertErr);
+              return res.status(500).send('Erro ao salvar usuário.');
+            }
+            res.send('Usuário Salvo!');
+          }
+        );
+      }
     }
-  });
+  );
 });
-
 
 app.delete('/api/usuarios/:id/:grupo', (req, res) => {
   const id = req.params.id;
   const grupo = req.params.grupo;
-  connection.query('DELETE FROM usuarios WHERE telegram_id = ? AND grupo = ?', [id, grupo], (err, result) => {
-    if (err) throw err;
-    res.send('Usuario Excluído!');
-  });
+
+  pool.query(
+    'DELETE FROM usuarios WHERE telegram_id = ? AND grupo = ?', 
+    [id, grupo], 
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Erro ao excluir usuário.');
+      }
+      res.send('Usuario Excluído!');
+    }
+  );
 });
+
 const port = 3002;
 app.listen(port, () => {
   console.log(`API rodando em http://localhost:${port}`);
